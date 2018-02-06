056cff945cc5914b3e2f9f02740f468869444e70
D'arteweb <contact@darteweb.com>
2018-02-06 11:41:28 +0530

    Update d-image-360.php

--- d-image-360.php
+++ d-image-360.php
@@ -4,20 +4,13 @@ Plugin Name: DImage 360
 Plugin URI: http://www.darteweb.com/
 Version: 1.1
 Author: D'arteweb
-Author URI: http://www.darteweb.com/
-Description: DImage 360 Slider
+Description: DImage 360 Slider use shortcode [dimage url="http://www.darteweb.com/images/Prague_Getty.jpg"]
 */
-
-define('d_path',plugin_dir_path( __FILE__));
-define('site_url',site_url());
-define('plugin_url',plugin_dir_url(__FILE__));
-
-
 function wp_dimage_msg_form_scripts() {
    
-    wp_enqueue_style('d-style', plugin_url."css/d-style.css");
-	wp_enqueue_script('photo-sphere-js1', plugin_url.'js/photo-sphere-viewer.min.js',array('jquery'), true);
-    wp_enqueue_script('three-min', plugin_url.'js/three.min.js',array('jquery'), true);
+    wp_enqueue_style('d-style', plugin_dir_url(__FILE__)."css/d-style.css");
+	wp_enqueue_script('photo-sphere-js1', plugin_dir_url(__FILE__).'js/photo-sphere-viewer.min.js',array('jquery'), true);
+    wp_enqueue_script('three-min', plugin_dir_url(__FILE__).'js/three.min.js',array('jquery'), true);
 }
 add_action('wp_enqueue_scripts','wp_dimage_msg_form_scripts');
 
@@ -28,8 +21,8 @@ $atts = shortcode_atts(
 	array(
 		'url' => '',
 	), $atts, 'dimage' );	
-	
 ob_start();
+
 ?>
 <div id="dtcontainer"></div>
 <script>
